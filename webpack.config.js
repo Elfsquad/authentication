1e268c7a8f4fd7a8b5156cb7ff62cf255854fe78
stanvanrooy <stanvanrooy@users.noreply.github.com>
2021-08-13 14:24:34 +0000

    Prettified Code!

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         use: [
-            {
-                loader: 'expose-loader',
-                options: {
-                    exposes: ['Elfsquad']
-                }
+          {
+            loader: "expose-loader",
+            options: {
+              exposes: ["Elfsquad"],
             },
-            {
-                loader: 'ts-loader'
-            }
+          },
+          {
+            loader: "ts-loader",
+          },
         ],
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
