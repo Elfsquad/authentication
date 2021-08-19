560757831316caf5a75f1ceb1e3af0530e79c786
Johannes Heesterman <johannes@elfsquad.io>
2021-08-19 16:47:59 +0200

    Change output file in webpack to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
 };
