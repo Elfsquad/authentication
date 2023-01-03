68ce84d2a886b5f8428e7f0a2007c2fb1d669a5e
Stan van Rooy <me@stanvanrooy.com>
2023-01-03 15:49:23 +0100

    feat: ability to give extra params to oauth request

--- src/authentication/authenticationContext.ts
+++ src/authentication/authenticationContext.ts
@@ -64,9 +64,9 @@ export class AuthenticationContext {
         return promise;
     }
 
-    public async signIn(): Promise<void> {
+    public async signIn(options: object): Promise<void> {
         await this.fetchConfiguration();
-        this.makeAuthorizationRequest();
+        this.makeAuthorizationRequest(options);
     }
 
     public signOut(postLogoutRedirectUri: string | null = null) {
@@ -112,7 +112,6 @@ export class AuthenticationContext {
         });
 
         const response = await this.tokenHandler.performRevokeTokenRequest(this.configuration, request);
-        console.log(tokenType, response);
         if (!response) {
             console.error(`Revoke token request for token '${tokenType}' failed`);
         }
@@ -214,7 +213,8 @@ export class AuthenticationContext {
         this.configuration = await AuthorizationServiceConfiguration.fetchFromIssuer(this.loginUrl, this.fetchRequestor);
     }
 
-    private makeAuthorizationRequest() {
+    private makeAuthorizationRequest(options: object) {
+        const extras = { 'access_type': 'offline', 'response_mode': 'fragment' };
         // create a request
         const request = new AuthorizationRequest({
             client_id: this.options.clientId,
@@ -222,7 +222,7 @@ export class AuthenticationContext {
             scope: this.options.scope,
             response_type: AuthorizationRequest.RESPONSE_TYPE_CODE,
             state: undefined,
-            extras: { 'access_type': 'offline', 'response_mode': 'fragment' }
+            extras: { ...extras, ...options }
         });
 
         // make the authorization request
@@ -231,6 +231,7 @@ export class AuthenticationContext {
 
     private async onAuthorization(request: AuthorizationRequest, response: AuthorizationResponse, error: AuthorizationError): Promise<void> {
         let locationVariable = window.location.href;
+
         location.hash = '';
         if (!!error) {
             for (let onSignInRejector of this.onSignInRejectors) {
