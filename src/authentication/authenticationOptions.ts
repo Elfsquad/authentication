163a5833dbce58706752cef770639c18f16c4021
Stan van Rooy <me@stanvanrooy.com>
2024-03-22 00:18:13 +0100

    feat: add tsdoc to support auto generated docs

--- src/authentication/authenticationOptions.ts
+++ src/authentication/authenticationOptions.ts
@@ -1,7 +1,22 @@
 export interface IAuthenticationOptions{
+    /**
+     * The client ID of your OpenID Connect application.
+    */
     clientId: string;
+    /**
+     * The url to redirect to after a login.
+    */
     redirectUri: string;
+    /**
+     * The oauth scopes to request, defaults to 'Elfskot.Api offline_access'.
+    */
     scope?: string | undefined;
+    /**
+     * The login url to use, defaults to 'login.elfsquad.io'.
+    */
     loginUrl?: string | undefined;
+    /**
+     * The response mode to use, defaults to 'fragment'.
+    */
     responseMode?: 'query' | 'fragment' | undefined;
 }
--- src/authentication/oauthOptions.ts
+++ src/authentication/oauthOptions.ts
@@ -1,10 +1,34 @@
 export interface IOauthOptions {
+  /**
+   * Is used to set the prompt parameter in the authorization request.
+   * By setting this parameter to none, the user will not be prompted to
+   * login, but instead be redirected back to the redirectUri with an error.
+   *
+   * This can be used to check if a user is already logged in or not.
+   *
+   * Defaults to 'login'.
+  */
   prompt?: 'none' | 'login' | 'consent' | 'select_account';
+
+  /**
+   * Is used to set the nonce parameter.
+  */
   nonce?: string;
   display?: 'page' | 'popup' | 'touch' | 'wap';
+
+  /**
+   * The max_age parameter is used to set the maximum age of the authentication
+   * session in seconds. If the user has been authenticated for longer than this
+   * time, the user will be prompted to login again.
+  */
   max_age?: number;
   ui_locales?: string;
   id_token_hint?: string;
+
+  /**
+   * The login_hint parameter is used to pre-fill the username field in the login form.
+   * This can be used to make the login process easier for the user.
+  */
   login_hint?: string;
   acr_values?: string;
 }
